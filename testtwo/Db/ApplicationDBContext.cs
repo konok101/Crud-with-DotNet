499a28b2cf8bbd7e0dabb34a8a02ecac4b60a2ab
konok <rockykonok@gmail.com>
2023-11-07 16:11:22 +0600

    jhjh

--- testtwo/Db/ApplicationDBContext.cs
+++ testtwo/Db/ApplicationDBContext.cs
@@ -6,7 +6,7 @@ namespace testtwo.Db
     public class ApplicationDBContext :DbContext
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
-
+        //fghf
         public DbSet<AttendenceModel> StudentAttendenuuuuces { get; set; }
 
     }
